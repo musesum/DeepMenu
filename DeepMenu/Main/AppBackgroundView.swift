0236b2f23c63020d81836e2a79fb0c1eea9cabba
Dav <dav@akuaku.org>
2022-02-22 10:36:04 -0800

    Actually update app background and border colors

--- DeepMenu/Main/AppBackgroundView.swift
+++ DeepMenu/Main/AppBackgroundView.swift
@@ -4,19 +4,22 @@ import Foundation
 
 import SwiftUI
 
-class MuSpace: ObservableObject {
-    @Published var color = Color(red: 0.2, green: 0.0, blue: 0.0, opacity: 1.00)
+class AppSpace: ObservableObject {
+    @Published var backgroundColor = Color(red: 0.2, green: 0.0, blue: 0.0, opacity: 1.00)
+    @Published var borderColor = Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 1.00)
+    @Published var borderWidth = 2.0
     init() {}
 }
 
 /// placeholder dark red background
 struct AppBackgroundView: View {
 
-    @ObservedObject var space: MuSpace
+    @ObservedObject var space: AppSpace
 
     var body: some View {
         Rectangle()
-            .foregroundColor(space.color)
+            .foregroundColor(space.backgroundColor)
+            .border(space.borderColor, width: space.borderWidth)
             .ignoresSafeArea(.all, edges: .all)
             .allowsHitTesting(false)
     }
