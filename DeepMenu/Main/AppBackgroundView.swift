5d422c12c1ca943f9e2c5ecb34f079c5ec1ccd24
ws <musesum@gmail.com>
2022-02-22 11:51:07 -0800

    rename MuSpace to AppBackground

--- DeepMenu/Main/AppBackgroundView.swift
+++ DeepMenu/Main/AppBackgroundView.swift
@@ -4,7 +4,7 @@ import Foundation
 
 import SwiftUI
 
-class MuSpace: ObservableObject {
+class AppBackground: ObservableObject {
     @Published var color = Color(red: 0.2, green: 0.0, blue: 0.0, opacity: 1.00)
     init() {}
 }
@@ -12,11 +12,11 @@ class MuSpace: ObservableObject {
 /// placeholder dark red background
 struct AppBackgroundView: View {
 
-    @ObservedObject var space: MuSpace
+    @ObservedObject var background: AppBackground
 
     var body: some View {
         Rectangle()
-            .foregroundColor(space.color)
+            .foregroundColor(background.color)
             .ignoresSafeArea(.all, edges: .all)
             .allowsHitTesting(false)
     }
