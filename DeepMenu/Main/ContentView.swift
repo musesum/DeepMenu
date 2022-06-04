124c2dc3a4c90a9b1ebd999d261177a363b15083
musesum <musesum@gmail.com>
2022-06-04 16:54:47 -0700

    ContentVm file

--- DeepMenu/Main/ContentView.swift
+++ DeepMenu/Main/ContentView.swift
@@ -12,32 +12,6 @@ struct ContentViews {
     static let client = ExampleClientView()
 }
 
-class ContentVm {
-
-    var skyNodes: [MuNode]
-    var skyTouchVm: MuTouchVm
-    var skyRootVm: MuRootVm
-    var skyTreeVm: MuTreeVm?
-    var skyBranchVm: MuBranchVm
-
-    init() {
-        // init in sequence: nodes, root, tree, branch, touch
-        skyNodes = ExampleTr3Sky.skyNodes()
-        skyRootVm = MuRootVm([.lower, .left], axii: [.vertical])
-        skyTreeVm = skyRootVm.treeSpotVm
-        skyBranchVm = MuBranchVm(nodes: skyNodes, treeVm: skyTreeVm, type: .node)
-        skyTreeVm?.addBranch(skyBranchVm)
-        skyTouchVm = skyRootVm.touchVm
-    }
-    
-    private func testBranches(_ treeVm: MuTreeVm) -> [MuBranchVm] {
-        let numberNodes = ExampleNodeModels.numberedNodes(5, numLevels: 5)
-        let letterNodes = ExampleNodeModels.letteredNodes()
-        let branches = [MuBranchVm(nodes: numberNodes, treeVm: treeVm, type: .node),
-                        MuBranchVm(nodes: letterNodes, treeVm: treeVm, type: .node)]
-        return branches
-    }
-}
 
 struct ContentView: View {
     let appSpace = AppSpace()
---
+++ DeepMenu/Main/ContentVm.swift
@@ -0,0 +1,31 @@
+//  Created by warren on 6/4/22.
+
+import SwiftUI
+
+class ContentVm {
+
+    var skyNodes: [MuNode]
+    var skyTouchVm: MuTouchVm
+    var skyRootVm: MuRootVm
+    var skyTreeVm: MuTreeVm?
+    var skyBranchVm: MuBranchVm
+
+    init() {
+        // init in sequence: nodes, root, tree, branch, touch
+        skyNodes = ExampleTr3Sky.skyNodes()
+        skyRootVm = MuRootVm([.lower, .left], axii: [.vertical])
+        skyTreeVm = skyRootVm.treeSpotVm
+        skyBranchVm = MuBranchVm(nodes: skyNodes, treeVm: skyTreeVm, type: .node)
+        skyTreeVm?.addBranch(skyBranchVm)
+        skyTouchVm = skyRootVm.touchVm
+    }
+
+    private func testBranches(_ treeVm: MuTreeVm) -> [MuBranchVm] {
+        let numberNodes = ExampleNodeModels.numberedNodes(5, numLevels: 5)
+        let letterNodes = ExampleNodeModels.letteredNodes()
+        let numberBranch = MuBranchVm(nodes: numberNodes, treeVm: treeVm, type: .node)
+        let letterBranch = MuBranchVm(nodes: letterNodes, treeVm: treeVm, type: .node)
+        let branches = [numberBranch, letterBranch]
+        return branches
+    }
+}
